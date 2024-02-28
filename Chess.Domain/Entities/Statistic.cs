92802b43378c3aa8f1b0e7daac53b46d2a9c7908
vahesanosyan <vahesanosyan@gmail.com>
2024-02-28 20:20:09 +0400

    change

--- Chess.Domain/Entities/Statistic.cs
+++ Chess.Domain/Entities/Statistic.cs
@@ -7,19 +7,19 @@ using System.Threading.Tasks;
 
 namespace Chess.Domain.Entities
 {
-    public class Stats
+    public class Statistic
     {
-        public string Id { get; set; }
-        public Gametype Type { get; set; }
-        public int GanesPlayed { get; set; }
+        public int Id { get; set; }
+        public GameType Type { get; set; }
+        public int GamesPlayed { get; set; }
         public int Wins {  get; set; }
         public int Losses {  get; set; }
         public int Draws { get; set; }
-        public int Ratings {  get; set; }
-        public DateTime CreateAt { get; set; } 
-        public DateTime UpdateAt { get; set; }
+        public int Rating {  get; set; }
+        public DateTime CreatedAt { get; set; } 
+        public DateTime UpdatedAt { get; set; }
         public User User { get; set; }
-        public string UserId { get; set; }
+        public int UserId { get; set; }
 
     }
 }
