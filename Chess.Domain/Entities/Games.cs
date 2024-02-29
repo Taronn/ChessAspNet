c95dd8510ff4de94edc53b230163f8c1543ff13c
vahesanosyan <vahesanosyan@gmail.com>
2024-02-29 21:27:32 +0400

    Games-Entity

---
+++ Chess.Domain/Entities/Games.cs
@@ -0,0 +1,25 @@
+﻿using Chess.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Domain.Entities
+{
+    public class Games
+    {
+        public int Id { get; set; } 
+        public GameType Type { get; set; }
+        public int WhitePlayerId { get; set; }
+        public int BlackPlayerId { get; set; }
+        public int WinnerId { get; set; }
+        public string Pgn { get; set; }
+        public int Timer {  get; set; }
+        public int TimerIncrement {  get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
