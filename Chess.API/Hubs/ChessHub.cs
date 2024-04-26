f8feac0cf1cfc8bd257176f317e0155674a9003c
Samvel Danielyan <samvel13danielyan@gmail.com>
2024-04-26 12:23:15 +0400

    MakeMove

--- Chess.API/Hubs/ChessHub.cs
+++ Chess.API/Hubs/ChessHub.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.SignalR;
 using Chess.Domain.Entities;
 using Chess.Application.Interfaces.Caching;
 using System.ComponentModel;
+using static Chess.Application.Services.GameService;
 namespace Chess.API.Hubs;
 
 [Authorize]
@@ -41,13 +42,26 @@ public class ChessHub : Hub
     {
         Player[] players = _playerService.FindAll();
         Player player = await _playerService.Join(UserId);
+        Game game = _gameService.Find(UserId);
         await Clients.User(UserId.ToString()).SendAsync("GetPlayersList", players);
         await Clients.Others.SendAsync("PlayerJoin", player);
+        
+       /* await Clients.Caller.SendAsync("SetGame", game.Board.ToPgn(), game.WhitePlayer, game.BlackPlayer);
+        Player opponent = _gameService.GetOpponent(player);
+        if (opponent == null)
+        {
+            await Clients.Caller.SendAsync("OpponentDisconnected");
+        }
+        else
+        {
+            await Clients.User(opponent.Id.ToString()).SendAsync("OpponentConnected");
+        }*/
     }
 
     public override async Task OnDisconnectedAsync(Exception exception)
     {
         _playerService.Remove(UserId);
+        _inviteCache.Remove(UserId);
         await Clients.Others.SendAsync("PlayerLeave", UserId);
     }
 
@@ -77,33 +91,98 @@ public class ChessHub : Hub
     }
 
     // for testing purposes only - remove later
-    public async Task MakeMove(string from, string to, string opponentId)
+    /*  public async Task MakeMove(string from, string to, string opponentId)
+      {
+          System.Console.WriteLine($"Move from {from} to {to} {opponentId}");
+          System.Console.WriteLine(opponentId);
+          System.Console.WriteLine(UserId);
+          await Clients.User(opponentId).SendAsync("MakeMove", from, to);
+      }*/
+   /* private async Task HandleEndGame()
     {
-        System.Console.WriteLine($"Move from {from} to {to} {opponentId}");
-        System.Console.WriteLine(opponentId);
-        System.Console.WriteLine(UserId);
-        await Clients.User(opponentId).SendAsync("MakeMove", from, to);
-    }
-    /*public async Task MakeMove(string from, string to)
+        Player Player = _playerService.Find(UserId);
+        Player Opponent = _gameService.GetOpponent(Player);
+        IClientProxy player = Clients.User(Player.Id.ToString());
+        IClientProxy opponent = Clients.User(Opponent?.Id.ToString() ?? "");
+        IClientProxy players = Clients.Users(UserId, Opponent?.Id.ToString() ?? "");
+        EndgameType? endGameType = await _gameService.EndGameAsync(Player);
+
+        await (endGameType switch
+        {
+            EndgameType.Checkmate => Task.WhenAll(
+                player.SendAsync("Win", "checkmate"),
+                opponent.SendAsync("Lose", "checkmate")
+            ),
+            EndgameType.Resigned => Task.WhenAll(
+                opponent.SendAsync("Win", "resignation"),
+                player.SendAsync("Lose", "resignation")
+            ),
+            EndgameType.DrawDeclared => players.SendAsync("Draw", "draw declaration"),
+            EndgameType.Stalemate => players.SendAsync("Draw", "stalemate"),
+            EndgameType.FiftyMoveRule => players.SendAsync("Draw", "fifty move rule"),
+            EndgameType.InsufficientMaterial => players.SendAsync("Draw", "insufficient material"),
+            EndgameType.Repetition => players.SendAsync("Draw", "repetition"),
+            _ => Task.CompletedTask
+        });
+    }*/
+
+    public async Task MakeMove(string from, string to)
     {
-        if (Game == null)
+        Player player = _playerService.Find(UserId);
+        Game game = _gameService.Find(UserId);
+        Player opponent = _gameService.GetOpponent(player);
+        if (game == null)
         {
             return;
         }
-
-        MoveResultType moveResult = _gameService.MakeMove(Player, Game, from, to);
+        MoveResultType moveResult = _gameService.MakeMove(player, game, from, to);
         switch (moveResult)
         {
             case MoveResultType.ValidMove:
-                await Clients.User(Opponent?.Id ?? "").SendAsync("MakeMove", from, to);
+                await Clients.User(opponent.Id.ToString()).SendAsync("MakeMove", from, to);
                 break;
             case MoveResultType.EndGame:
-                await Clients.User(Opponent?.Id ?? "").SendAsync("MakeMove", from, to);
-                await HandleEndGame();
-                break;
+                await Clients.User(opponent.Id.ToString()).SendAsync("MakeMove", from, to);
+/*                await HandleEndGame();
+*/                break;
             case MoveResultType.InvalidMove:
                 await Clients.Caller.SendAsync("InvalidMove");
                 break;
         }
-    }*/
+    }
+    public async Task Resign()
+    {
+        Player player = _playerService.Find(UserId);
+        if (_gameService.Resign(player))
+        {
+/*            await HandleEndGame();
+*/        }
+    }
+    public async Task OfferDraw()
+    {
+        Player player = _playerService.Find(UserId);
+        Player opponent = _gameService.GetOpponent(player);
+        if (_gameService.OfferDraw(player))
+        {
+            await Clients.User(opponent!.Id.ToString()).SendAsync("DrawOfferReceived");
+        }
+    }
+
+    public async Task AcceptDraw()
+    {
+        Player player = _playerService.Find(UserId);
+        if (_gameService.AcceptDraw(player))
+        {
+/*            await HandleEndGame();
+*/        }
+    }
+    public async Task RejectDraw()
+    {
+        Player player = _playerService.Find(UserId);
+        Player opponent = _gameService.GetOpponent(player);
+        if (_gameService.RejectDraw(player))
+        {
+            await Clients.User(opponent?.Id.ToString() ?? "").SendAsync("DrawOfferRejected");
+        }
+    }
 }
--- Chess.Application/Interfaces/Services/IGameService.cs
+++ Chess.Application/Interfaces/Services/IGameService.cs
@@ -1,3 +1,5 @@
+using static Chess.Application.Services.GameService;
+
 namespace Chess.Application.Interfaces.Services;
 
 public interface IGameService
@@ -5,4 +7,13 @@ public interface IGameService
     Game Create(Invite invite);
     Game Find(Guid id);
     void Remove(Guid id);
+    Player GetOpponent(Player player, bool onlineOnly = true);
+/*    async Task<EndgameType?> EndGameAsync(Player player);
+*/    bool IsPlayerTurn(Player player, Game game);
+    bool Resign(Player player);
+    bool OfferDraw(Player player);
+/*    async Task UpdateStats(Game game);
+*/    bool AcceptDraw(Player player);
+    bool RejectDraw(Player player);
+    MoveResultType MakeMove(Player player, Game game, string from, string to);
 }
